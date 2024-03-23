e669ef2677a95b626b26a4adbd98bab5e1a56ade
tdupuy <tom.dupuy@4sh.fr>
2024-03-23 20:48:50 +0100

    fix merge issue

--- services/config.service.js
+++ services/config.service.js
@@ -8,8 +8,8 @@ export const ConfigService = {
             }
         },
         keys: {
-            API_TOKEN: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNjg1NGVlYTk3MjI0YWYxOTA3ZTEyNmFkOTA4YTIzOSIsInN1YiI6IjY1ZTliNzc3NmJlYWVhMDE4NjdhM2RjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aD2Z9pJ-9PdxzXJ8KLgDcU2H5winwCmH-dc64_y3SQQ",
-            API_KEY: "06854eea97224af1907e126ad908a239"
+            API_TOKEN: "changeMe",
+            API_KEY: "changeMe"
         }
     },
     youtube: {
