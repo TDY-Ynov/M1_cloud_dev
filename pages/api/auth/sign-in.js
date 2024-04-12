e9707f9e0e8d28fdf75feecfaeb9eba2eff8cd2c
tdupuy <tom.dupuy@4sh.fr>
2024-04-12 21:26:53 +0200

    check with data in dbb for sign-in

--- pages/api/auth/sign-in.js
+++ pages/api/auth/sign-in.js
@@ -1,6 +1,22 @@
-export default async function handler(req, res) {
-    const { email, password } = req.body
-    console.log(email, password);
+import clientPromise from "../../../lib/mongodb";
 
-    res.status(200).json({ success: true, userData: {name: "John"} });
+export default async function handler(req, res) {
+    const client = await clientPromise;
+    const db = client.db("ynov-cloud");
+
+    if (req.method === "POST") {
+        try {
+            const user = await db.collection("users").findOne({ email: req.body.email });
+            if (!user || user.password !== req.body.password) {
+                return res.status(401).json({ status: 401, error: "Invalid email or password" });
+            }
+
+            res.status(200).json({ success: true, userData: { name: user.name } });
+        } catch (error) {
+            console.error("Error authenticating user:", error);
+            res.status(500).json({ status: 500, error: "Internal Server Error" });
+        }
+    } else {
+        res.status(405).json({ status: 405, error: "Method Not Allowed" });
+    }
 }
